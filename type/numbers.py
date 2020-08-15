c414702a623596dc9267e43694c0888c4eb0891e
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-15 22:48:15 +0800

    Update numbers.py

--- type/numbers.py
+++ type/numbers.py
@@ -1,5 +1,32 @@
 >>> type(5)
 <class 'init'>
-
 >>> type(3.142)
 <class 'float'>
+>>> 5+5
+10
+>>> 5-5
+0
+>>> 5*5
+25
+>>> 5/5
+1.0
+>>> 5//5 #division in integer
+1
+>>> 5**5 #to the power of
+3125
+>>> age+=1
+>>> age
+21
+>>> age-=1
+>>> age
+20
+>>> age/=2
+>>> age
+10.0
+>>> wages=1000
+>>> bills=200
+>>> rent=500
+>>> food=200
+>>> savings=wages-bills-rent-food
+>>> savings
+100
