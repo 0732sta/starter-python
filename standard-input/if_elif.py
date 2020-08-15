6022c565b876a5aba6be149b4005b8d3995c6f46
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-16 03:45:22 +0800

    Add files via upload

---
+++ standard-input/if_elif.py
@@ -0,0 +1,18 @@
+#OPERATORS
+# <,>,==,!=,=>,=<
+
+#age = int(input('Enter your age'))
+# 
+#if age <10:
+#   print("you are young, stranger one")
+# elif age<40:
+#   print('the fire in you is strong,strange one')
+#else:
+#   print('you are wise beyond doubt,strange on')
+
+meaty=input('Do you eat meat?(yes/no):')
+
+if meaty =='y':
+    print('here is the meaty menu.')
+else:
+    print('here is the veggie menu.')
