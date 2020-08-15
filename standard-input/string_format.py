35e7eae7afcb337ddf16bad1b0a28cc9c6b0fa4a
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-16 03:28:15 +0800

    Add files via upload

---
+++ standard-input/string_format.py
@@ -0,0 +1,13 @@
+num1=3.142524
+num2=10.34864
+
+#print('num 1 is',num1,'and num 2 is',num2)
+
+#print('num 1 is {0} and num 2 is {1}'.format(num1,num2))
+
+#if we want to display specific decimal 
+print('num 1 is {0:.3} and num 2 is {1:.3}'.format(num1,num2)) 
+
+print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1,num2))
+#or you can write like this :
+print(f'num 1 is {num1:.3f} and num 2 is {num2:.3f}')
