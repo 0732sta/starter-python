f7a89551d76be1a8c66fdee4d2badd741f16458d
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-16 03:12:02 +0800

    Add files via upload

---
+++ standard-input/area_calc.py
@@ -0,0 +1,11 @@
+name=input('Tell me your name:')
+#print('salam '+name)
+age = input('age? ')
+print(name,'you are',age,'!!')
+
+#Calc the area of a circle
+#radius=input('Enter the radius of your circle (m):')
+#area=3.142*radius**2 
+# line-8 is error because you not declare the type of radius
+#area=3.142*int(radius)**2 
+#print("The area of your cicrle is:",area)
