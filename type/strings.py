40bada4187dd44d0576d9b4b0c16e30c118d21bd
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-15 23:38:55 +0800

    Update strings.py

--- type/strings.py
+++ type/strings.py
@@ -1,1 +1,39 @@
-
+>>> 'hello'
+'hello'
+>>> "go girl!"
+'go girl!'
+>>> "hello"
+'hello'
+>>> "she\'s humble"
+"she's humble"
+>>> greet='hello'
+>>> greet
+'hello'
+>>> greet[0]
+'h'
+>>> greet[1]
+'e'
+>>> greet[-1]
+'o'
+>>> greet[-5]
+'h'
+>>> greet[0:3]
+'hel'
+>>> greet[0:4]
+'hell'
+>>> greet=greet[0:3]
+>>> greet
+'hel'
+>>> add='dear'
+>>> greet+add
+'heldear'
+>>> greet="hello"
+>>> greet*5
+'hellohellohellohellohello'
+>>> greet.upper()
+'HELLO'
+>>> drink="coke,pepsi,zamzam"
+>>> drink.split(',')
+['coke', 'pepsi', 'zamzam']
+>>> len(greet)
+5
