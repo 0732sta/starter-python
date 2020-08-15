690b4f37751e2432d3f2fd76c4ed24707e939d5e
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-08-16 03:55:05 +0800

    Update if_elif.py

--- standard-input/if_elif.py
+++ standard-input/if_elif.py
@@ -5,7 +5,7 @@
 # 
 #if age <10:
 #   print("you are young, stranger one")
-# elif age<40:
+#elif age<40:
 #   print('the fire in you is strong,strange one')
 #else:
 #   print('you are wise beyond doubt,strange on')
