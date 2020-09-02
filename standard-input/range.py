5e8f12b2abbaf5512ba3ab9421bfa7ac7de0ebdd
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-09-02 16:18:42 +0800

    Add files via upload

---
+++ standard-input/range.py
@@ -0,0 +1,16 @@
+#for n in range(5):
+#    print(n)
+
+#for n in range(3,10):
+#    print(n)   
+
+#for n in range(0,20,4):
+#    print(n)   
+
+prayer=['subuh','zhor','asr','magrib','isya']
+
+#for n in range(len(burgers)):
+# print(n,burgers[n])
+
+for n in range(len(prayer)-1,-1,-1):
+    print(n,prayer[n])
