e62e189f3f18334df6fd3d1d5f9921c71e4be9e3
Atsilah Anuar <42581091+0732sta@users.noreply.github.com>
2020-09-21 00:43:47 +0800

    Update dl_img.py

--- project/dl_img.py
+++ project/dl_img.py
@@ -4,7 +4,7 @@ def dl_jpg(url,file_path,file_name):
     full_path=file_path + file_name + '.jpg'
     urllib.request.urlretrieve(url,full_path)
 
-url=input('Enter ing URL to dowload: ')
+url=input('Enter image URL to dowload: ')
 file_name =input('Enter file name to save as:')
 
 dl_jpg(url, 'images/',file_name)
